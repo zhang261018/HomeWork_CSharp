ed0d4c180a7e8a7349ede9f2214b0bb4b599854a
zhang261018 <z1328775371@126.com>
2021-03-06 09:42:20 +0800

    第一次作业提交

---
+++ HomeWork1/CalculatorInConsole/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace CalculatorInConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool countine = true;
+            string operators = "";
+            double num1 = 0, num2 = 0, ans = 0;
+
+            while (countine)
+            {
+                try
+                {
+                    //输入数据
+                    Console.WriteLine("Please input a number:");
+                    num1 = Double.Parse(Console.ReadLine().Trim());
+                    Console.WriteLine("Please input another number:");
+                    num2 = Double.Parse(Console.ReadLine().Trim());
+                    Console.WriteLine("Please input the operator(+,-,*,/,%):");
+                    operators = Console.ReadLine().Trim();
+
+                    //根据输入运算符进行相应操作
+                    switch (operators)
+                    {
+                        case "+": ans = num1 + num2; break;
+                        case "-": ans = num1 - num2; break;
+                        case "*": ans = num1 * num2; break;
+                        //C#除0得到无穷，故此处不进行除零的异常处理
+                        case "/": ans = num1 / num2; break;
+                        case "%":
+                            {
+                                if (Math.Abs(num2) >= 1e-7)
+                                    ans = num1 % num2;
+                                else
+                                    throw new DivideByZeroException("mod zero Exception.");
+                            }
+                            break;
+                        default:
+                            throw new Exception("input a correct operator.");
+                    }
+
+                    Console.WriteLine($"The answer is {ans}");
+                }
+                //输入非数字检测
+                catch (Exception e)
+                {
+                    Console.WriteLine("Please input a correct number or operator.");
+                }
+
+                //输入是否继续，单词输入错误默认继续
+                try
+                {
+                    Console.WriteLine("do you want to countine? (true or false)");
+                    countine = Boolean.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Input a valid word please.");
+                    countine = true;
+                }
+
+
+                //等待一段时间并清空控制台为下一次输入做准备
+                Thread.Sleep(2000);
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ HomeWork1/CalculatorInWindows/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorInWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Form1_Load();
+        }
+
+        //为comboBox添加初始选项
+        public void Form1_Load()
+        {
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("*");
+            comboBox1.Items.Add("/");
+            comboBox1.Items.Add("%");
+        }
+
+        //链接计算按钮的槽函数
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = 0, num2 = 0;
+            string _operator = comboBox1.SelectedItem.ToString().Trim();
+
+            //获取用户输入的变量与选择的运算
+            try
+            {
+                num1 = Double.Parse(textBox1.Text.Trim());
+                num2 = Double.Parse(textBox2.Text.Trim());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Input correct numbers plz.");
+                return;
+            }
+
+            //判断运算种类
+            try
+            {
+                switch (_operator)
+                {
+                    case "+": textBox4.Text = $"{num1 + num2}"; break;
+                    case "-": textBox4.Text = $"{num1 - num2}"; break;
+                    case "*": textBox4.Text = $"{num1 * num2}"; break;
+                    //C#除以0会得到无穷，这是我们想要的结果，故此处不抛出异常
+                    case "/": textBox4.Text = $"{num1 / num2}"; break;
+                    case "%":
+                        {
+                            if (Math.Abs(num2) >= 1e-7)
+                                textBox4.Text = $"{num1 % num2}";
+                            else
+                                throw new DivideByZeroException("mod zero Exception.");
+                        }
+                        break;
+                }
+            }
+            //模零异常处理
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("you can't mod zero.");
+                textBox4.Text = Double.NaN.ToString();
+            }
+        }
+
+        //链接清空按钮的槽函数
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox4.Text = "";
+        }
+
+    }
+}
