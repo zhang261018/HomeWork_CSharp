dae1897093aab015c9d5f94695b530cdbedd30e9
zhang261018 <z1328775371@126.com>
2021-03-24 10:07:12 +0800

    修改了部分不合理之处

--- HomeWork3/GraphicsImplement/Factory.cs
+++ HomeWork3/GraphicsImplement/Factory.cs
@@ -10,29 +10,23 @@ namespace GraphicsFactory
         public static object GetProduct(String graphics, params double[] length)
         {
             Delineation obj = null;
-            try
+            //根据传入的类型返回不同的图形
+
+            switch (graphics.ToLower())
             {
-                //根据传入的类型返回不同的图形
-                switch (graphics.ToLower())
-                {
-                    case "square":obj = new Square(length[0]); break;
-                    case "rectangle":obj = new Rectangle(length[0], length[1]); break;
-                    case "triangle":obj = new Triangle(new double[3] { length[0], length[1], length[2] }); break;
-                    case "circle":obj = new Circle(length[0]); break;
-                    default: break;
-                }
-                if(obj == null)
-                {
-                    throw new ArgumentException("type error!");
-                }
-                if(!obj.IsLegal)
-                {
-                    throw new ArgumentException("argument error!");
-                }
+                case "square": obj = new Square(length[0]); break;
+                case "rectangle": obj = new Rectangle(length[0], length[1]); break;
+                case "triangle": obj = new Triangle(new double[3] { length[0], length[1], length[2] }); break;
+                case "circle": obj = new Circle(length[0]); break;
+                default: break;
             }
-            catch(Exception e)
+            if (obj == null)
             {
-                Console.WriteLine(e.Message);
+                throw new ArgumentException("type error!");
+            }
+            if (!obj.IsLegal)
+            {
+                throw new ArgumentException("argument error!");
             }
 
             return obj;
